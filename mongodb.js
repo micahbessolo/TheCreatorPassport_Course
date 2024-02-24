4bfb21bc14ce5a53ccb44473c9287ae17af218af
Micah Bessolo <83624003+micahbessolo@users.noreply.github.com>
2024-02-24 07:51:38 -0800

    Big update

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/LoginSignup")
-.then(() => {
+// mongoose.connect("mongodb://127.0.0.1:27017/LoginSignup")
+// mongoose.connect("mongodb+srv://master:mongodb-master@loginsignup.5mlwogs.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://bessolomicah:1Cdattcwsm.@loginsignup.5mlwogs.mongodb.net/?retryWrites=true&w=majority")
+
+.then(() => 
+{
     console.log("mongo connected");
 })
-.catch('error', err => {
+.catch('error', err => 
+{
     console.log("failed to connect to MongoDB: ", err);
 });
 
@@ -29,14 +34,160 @@ const LoginSchema = new mongoose.Schema({
         type: String,
         default: ''
     },
-    videoState: {
-        type:String,
-        default: ''
+    _1_1: {
+        type: Array,
+        default: ["","","","","","","","","","","","","",""]
+    },
+    _1_2: {
+        type: Array,
+        default: ["","","","","","","","","","","","","","","","",""] 
+    },
+    _1_3: {
+        type: Array,
+        default: ["","","","","","","","","",""]
+    },
+    _1_4: {
+        type: Array,
+        default: ["","","",""]
+    },
+    _1_5: {
+        type: Array,
+        default: ["","","","","",""]
+    },
+    _1_6: {
+        type: Array,
+        default: ["","","",""]
+    },
+    _1_7: {
+        type: Array,
+        default: ["","","","","","","","","",""]
+    },
+    _1_8: {
+        type: Array,
+        default: ["","",""]
+    },
+    _1_9: {
+        type: Array,
+        default: ["","","","","","","","","","","","","","","","","","","","","",""]
+    },
+    _1_10: {
+        type: Array,
+        default: ["","","","","","","","","","","","","","","","","","","","","","","","",""]
+    },
+    _1_11: {
+        type: Array,
+        default: ["","","","","","","",""]
+    },
+    _1_12: {
+        type: Array,
+        default: ["","","","","","","","","","","","","","","","","","","",""]
+    },
+    _1_13: {
+        type: Array,
+        default: ["","","","","","","","","","",""]
+    },
+    _1_14: {
+        type: Array,
+        default: ["","","","","","","","","","","","","","","",""]
+    },
+    _1_15: {
+        type: Array,
+        default: ["","","","","",""]
+    },
+    _1_16: {
+        type: Array,
+        default: ["","","","","","",""]
+    },
+    _2_1: {
+        type: Array,
+        default: ["","","","","","","",""]
+    },
+    _2_2: {
+        type: Array,
+        default: ["","","","","","","","","",""]
+    },
+    _2_3: {
+        type: Array,
+        default: ["","","","",""]
+    },
+    _2_4: {
+        type: Array,
+        default: ["","","","","",""]
+    },
+    _2_5: {
+        type: Array,
+        default: ["","","",""]
+    },
+    _2_6: {
+        type: Array,
+        default: ["","","","","","",""]
+    },
+    _2_7: {
+        type: Array,
+        default: ["",""]
+    },
+    _2_8: {
+        type: Array,
+        default: ["",""]
+    },
+    _2_9: {
+        type: Array,
+        default: ["","","","","",""]
+    },
+    _2_10: {
+        type: Array,
+        default: ["","","","","","",""]
+    },
+    _2_11: {
+        type: Array,
+        default: ["","","",""]
+    },
+    _3_1: {
+        type: Array,
+        default: ["","","","","","","","",""]
+    },
+    _3_2: {
+        type: Array,
+        default: ["","","","",""]
+    },
+    _3_3: {
+        type: Array,
+        default: ["","","",""]
+    },
+    _3_4: {
+        type: Array,
+        default: ["","","","","","",""]
+    },
+    _3_5: {
+        type: Array,
+        default: ["","","",""]
+    },
+    _3_6: {
+        type: Array,
+        default: ["","","","",""]
+    },
+    _3_7: {
+        type: Array,
+        default: ["","","","",""]
+    },
+    _3_8: {
+        type: Array,
+        default: ["","","","","","",""]
+    },
+    _3_9: {
+        type: Array,
+        default: ["","","",""]
+    },
+    _3_10: {
+        type: Array,
+        default: ["","","","","","","","",""]
     }
     
-}, {timestamps: true})
+}, {timestamps: true});
 
+const loginCollection = mongoose.model("logincollections", LoginSchema);
 
-const collection = mongoose.model("logincollections", LoginSchema);
-
-module.exports = collection;
+module.exports = 
+{
+    loginCollection: loginCollection
+}
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ if ((process.env.NODE_ENV || '').trim() !== 'production')
 }
 else
 {
-    console.log("we in prod boiii")
+    console.log("Production")
 }
 
 const express = require('express');
@@ -14,14 +14,14 @@ const passport = require('passport');
 const flash = require('express-flash');
 const session = require('express-session');
 const methodOverride = require('method-override');
-const collection = require("./mongodb");
+const {loginCollection} = require("./mongodb");
 const initializePassport = require('./passport-config');
 const {forgotPassword, resetPassword} = require("./password_recovery/auth");
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
 
 initializePassport(passport, 
-    email => collection.findOne({email: email}),
-    id => collection.findOne({_id: id})
+    email => loginCollection.findOne({email: email}),
+    id => loginCollection.findOne({_id: id})
 );
 
 app.set('view-engine', 'ejs');
@@ -39,48 +39,133 @@ app.use(methodOverride('_method'));
 
 let userEmail;
 
+let _1_1;let _1_2;let _1_3;let _1_4;let _1_5;let _1_6;let _1_7;let _1_8;let _1_9;let _1_10;
+let _1_11;let _1_12;let _1_13;let _1_14;let _1_15;let _1_16;let _2_1;let _2_2;let _2_3;
+let _2_4;let _2_5;let _2_6;let _2_7;let _2_8;let _2_9;let _2_10;let _2_11;let _3_1;let _3_2;
+let _3_3;let _3_4;let _3_5;let _3_6;let _3_7;let _3_8;let _3_9;let _3_10;
+
 app.get('/', checkAuthenticated, async (req, res) =>
 {
     let videoTime;
-    const userName = await collection.findOne({_id: req.user._conditions._id}).then((user, err) =>
+    const userName = await loginCollection.findOne({_id: req.user._conditions._id}).then((user, err) =>
     {
         userEmail = user.email;
-        videoTime = user.videoState;
+        _1_1 = user._1_1;
+        _1_2 = user._1_2;
+        _1_3 = user._1_3;
+        _1_4 = user._1_4;
+        _1_5 = user._1_5;
+        _1_6 = user._1_6;
+        _1_7 = user._1_7;
+        _1_8 = user._1_8;
+        _1_9 = user._1_9;
+        _1_10 = user._1_10;
+        _1_11 = user._1_11;
+        _1_12 = user._1_12;
+        _1_13 = user._1_13;
+        _1_14 = user._1_14;
+        _1_15 = user._1_15;
+        _1_16 = user._1_16;
+        _2_1 = user._2_1;
+        _2_2 = user._2_2;
+        _2_3 = user._2_3;
+        _2_4 = user._2_4;
+        _2_5 = user._2_5;
+        _2_6 = user._2_6;
+        _2_7 = user._2_7;
+        _2_8 = user._2_8;
+        _2_9 = user._2_9;
+        _2_10 = user._2_10;
+        _2_11 = user._2_11;
+        _3_1 = user._3_1;
+        _3_2 = user._3_2;
+        _3_3 = user._3_3;
+        _3_4 = user._3_4;
+        _3_5 = user._3_5;
+        _3_6 = user._3_6;
+        _3_7 = user._3_7;
+        _3_8 = user._3_8;
+        _3_9 = user._3_9;
+        _3_10 = user._3_10;
         return user.name;
     });
 
-    res.render('library.ejs', { name: userName, videoState: videoTime });
+    res.render('library.ejs', { 
+        name: userName, 
+        _1_1: _1_1,
+        _1_2: _1_2,
+        _1_3: _1_3,
+        _1_4: _1_4,
+        _1_5: _1_5,
+        _1_6: _1_6,
+        _1_7: _1_7,
+        _1_8: _1_8,
+        _1_9: _1_9,
+        _1_10: _1_10,
+        _1_11: _1_11,
+        _1_12: _1_12,
+        _1_13: _1_13,
+        _1_14: _1_14,
+        _1_15: _1_15,
+        _1_16: _1_16,
+        _2_1: _2_1,
+        _2_2: _2_2,
+        _2_3: _2_3,
+        _2_4: _2_4,
+        _2_5: _2_5,
+        _2_6: _2_6,
+        _2_7: _2_7,
+        _2_8: _2_8,
+        _2_9: _2_9,
+        _2_10: _2_10,
+        _2_11: _2_11,
+        _3_1: _3_1,
+        _3_2: _3_2,
+        _3_3: _3_3,
+        _3_4: _3_4,
+        _3_5: _3_5,
+        _3_6: _3_6,
+        _3_7: _3_7,
+        _3_8: _3_8,
+        _3_9: _3_9,
+        _3_10: _3_10
+    });
 });
 
-app.get('/login', checkNotAuthenticated, (req, res) => {
+app.get('/login', checkNotAuthenticated, (req, res) => 
+{
     res.render('login.ejs');
 });
 
-app.get('/forgot-password', (req, res) => {
+app.get('/forgot-password', (req, res) => 
+{
     res.render('forgot-password.ejs');
 });
 
 app.post('/forgot-password', forgotPassword);
 
-app.get('/reset-password', (req, res) => {
+app.get('/reset-password', (req, res) => 
+{
     res.render('reset-password.ejs');
 });
 
 app.post('/reset-password', resetPassword);
 
-app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', 
+{
     successRedirect: '/',
     failureRedirect: '/login',
     failureFlash: true
 }));
 
-app.get('/register', checkNotAuthenticated, (req, res) => {
+app.get('/register', checkNotAuthenticated, (req, res) => 
+{
     res.render('register.ejs');
 });
 
 app.post('/register', checkNotAuthenticated, (req, res) =>
 {
-    collection.findOne({email: req.body.email}).then(async (user, err) => 
+    loginCollection.findOne({email: req.body.email}).then(async (user, err) => 
     {
         if (user)
         {
@@ -95,10 +180,9 @@ app.post('/register', checkNotAuthenticated, (req, res) =>
                     createdDate: Date.now().toString(),
                     name: req.body.name,
                     email: req.body.email,
-                    password: hashedPassword,
-                    videoState: ''
+                    password: hashedPassword
                 }
-            await collection.insertMany([data]);
+            await loginCollection.insertMany([data]);
             res.redirect('/login');
         }
         catch
@@ -110,40 +194,92 @@ app.post('/register', checkNotAuthenticated, (req, res) =>
 
 app.patch('/video-state:videoState', async (req, res) =>
 {
-    console.log(userEmail);
-
     let password;
     let email; 
     let userName;
     let createdDate;
-
-    const results = await collection.find({email: userEmail}).then((info, err) =>
+    
+    const results = await loginCollection.find({email: userEmail}).then((info, err) =>
     {
         password = info[0].password;
         email = info[0].email;
         userName = info[0].name;
-        createdDate = info[0].createdDate
+        createdDate = info[0].createdDate;
+        _1_1 = info[0]._1_1;_1_2 = info[0]._1_2;_1_3 = info[0]._1_3;_1_4 = info[0]._1_4;
+        _1_5 = info[0]._1_5;_1_6 = info[0]._1_6;_1_7 = info[0]._1_7;_1_8 = info[0]._1_8;
+        _1_9 = info[0]._1_9;_1_10 = info[0]._1_10;_1_11 = info[0]._1_11;_1_12 = info[0]._1_12;
+        _1_13 = info[0]._1_13;_1_14 = info[0]._1_14;_1_15 = info[0]._1_15;_1_16 = info[0]._1_16;
+        _2_1 = info[0]._2_1;_2_2 = info[0]._2_2;_2_3 = info[0]._2_3;_2_4 = info[0]._2_4;
+        _2_5 = info[0]._2_5;_2_6 = info[0]._2_6;_2_7 = info[0]._2_7;_2_8 = info[0]._2_8;
+        _2_9 = info[0]._2_9;_2_10 = info[0]._2_10;_2_11 = info[0]._2_11;_3_1 = info[0]._3_1;
+        _3_2 = info[0]._3_2;_3_3 = info[0]._3_3;_3_4 = info[0]._3_4;_3_5 = info[0]._3_5;
+        _3_6 = info[0]._3_6;_3_7 = info[0]._3_7;_3_8 = info[0]._3_8;_3_9 = info[0]._3_9;
+        _3_10 = info[0]._3_10;
     });
 
-    const videoState = JSON.parse(JSON.stringify(req.body)).sectionTime;
+    const currentTime = JSON.parse(JSON.stringify(req.body)).currentTime;
+    const videoDuration = JSON.parse(JSON.stringify(req.body)).videoDuration;
+    const URLData = JSON.parse(JSON.stringify(req.body)).URLData;
+    const sectionArray = `_${URLData.split('-')[0]}_${URLData.split('-')[1]}`;
+    const Lesson = Number(URLData.split('-')[2]) - 1;
+    let percentageComplete = Number(currentTime)/Number(videoDuration);
+    percentageComplete = percentageComplete.toFixed(2);
+
+
+    eval(`_${URLData.split('-')[0]}_${URLData.split('-')[1]}[${Lesson}] = "${currentTime}_${videoDuration}_${percentageComplete}";`);
 
     try
     {
-        const data = 
+        let data = 
         {
             password: password,
             email: email,
             name: userName,
             createdDate: createdDate,
-            videoState: videoState
-        }
+            _1_1: _1_1,
+            _1_2: _1_2,
+            _1_3: _1_3,
+            _1_4: _1_4,
+            _1_5: _1_5,
+            _1_6: _1_6,
+            _1_7: _1_7,
+            _1_8: _1_8,
+            _1_9: _1_9,
+            _1_10: _1_10,
+            _1_11: _1_11,
+            _1_12: _1_12,
+            _1_13: _1_13,
+            _1_14: _1_14,
+            _1_15: _1_15,
+            _1_16: _1_16,
+            _2_1: _2_1,
+            _2_2: _2_2,
+            _2_3: _2_3,
+            _2_4: _2_4,
+            _2_5: _2_5,
+            _2_6: _2_6,
+            _2_7: _2_7,
+            _2_8: _2_8,
+            _2_9: _2_9,
+            _2_10: _2_10,
+            _2_11: _2_11,
+            _3_1: _3_1,
+            _3_2: _3_2,
+            _3_3: _3_3,
+            _3_4: _3_4,
+            _3_5: _3_5,
+            _3_6: _3_6,
+            _3_7: _3_7,
+            _3_8: _3_8,
+            _3_9: _3_9,
+            _3_10: _3_10
+        };
 
-        const newState = await collection.findOneAndUpdate({email: email},data, {new: true});
-        console.log(newState)
+        const newState = await loginCollection.findOneAndUpdate({email: email}, data, {new: true});
     }
-    catch
+    catch(err)
     {
-        console.log("didn't work");
+        console.log("didn't work: " + err);
     }
 
     return res.json({
@@ -181,11 +317,13 @@ function checkNotAuthenticated(req, res, next)
 
 //payment setup
 
-app.get('/success', (req, res) => {
+app.get('/success', (req, res) => 
+{
     res.render('success.ejs');
 });
 
-app.get('/cancel', (req, res) => {
+app.get('/cancel', (req, res) => 
+{
     res.render('cancel.ejs');
 });
 
@@ -193,41 +331,231 @@ const storeItems = new Map([
     [1, { priceInCents: 300000, name: "course" }]
 ]);
 
-app.get('/create-checkout-session', (req, res) => {
+app.get('/create-checkout-session', (req, res) => 
+{
     res.render('checkout.ejs');
 })
 
-app.post('/create-checkout-session', async (req, res) => {
-   try {
-    const session = await stripe.checkout.sessions.create(
+app.post('/create-checkout-session', async (req, res) => 
+{
+    try 
     {
-        payment_method_types: ['card'],
-        mode: 'payment',
-        line_items: req.body.items?.map(item => 
+        const session = await stripe.checkout.sessions.create(
         {
-            const  storeItem = storeItems.get(item.id)
-            return {
-                price_data: {
-                    currency: 'usd',
-                    product_data: {
-                        name: storeItem.name
+            payment_method_types: ['card'],
+            mode: 'payment',
+            line_items: req.body.items?.map(item => 
+            {
+                const  storeItem = storeItems.get(item.id)
+                return {
+                    price_data: {
+                        currency: 'usd',
+                        product_data: {
+                            name: storeItem.name
+                        },
+                        unit_amount: storeItem.priceInCents
                     },
-                    unit_amount: storeItem.priceInCents
-                },
-                quantity: item.quantity
-            }
-        }),
-        success_url: `${process.env.CLIENT_URL}/success`,
-        cancel_url: `${process.env.CLIENT_URL}/cancel`,
-    });
-    res.json({ url: session.url });
-   } catch (e) {
-    res.status(500).json({ error: e.message });
-   }
+                    quantity: item.quantity
+                }
+            }),
+            success_url: `${process.env.CLIENT_URL}/success`,
+            cancel_url: `${process.env.CLIENT_URL}/cancel`,
+        });
+        res.json({ url: session.url });
+    }
+    catch (e)
+    {
+        res.status(500).json({ error: e.message });
+    }
 });
 
-app.get('/course', (req, res) => {
-    res.render('course.ejs');
+app.get('/course', async (req, res) => 
+{
+
+    const userName = await loginCollection.findOne({_id: req.user._conditions._id}).then((user, err) =>
+    {
+        userEmail = user.email;
+        _1_1 = user._1_1;
+        _1_2 = user._1_2;
+        _1_3 = user._1_3;
+        _1_4 = user._1_4;
+        _1_5 = user._1_5;
+        _1_6 = user._1_6;
+        _1_7 = user._1_7;
+        _1_8 = user._1_8;
+        _1_9 = user._1_9;
+        _1_10 = user._1_10;
+        _1_11 = user._1_11;
+        _1_12 = user._1_12;
+        _1_13 = user._1_13;
+        _1_14 = user._1_14;
+        _1_15 = user._1_15;
+        _1_16 = user._1_16;
+        _2_1 = user._2_1;
+        _2_2 = user._2_2;
+        _2_3 = user._2_3;
+        _2_4 = user._2_4;
+        _2_5 = user._2_5;
+        _2_6 = user._2_6;
+        _2_7 = user._2_7;
+        _2_8 = user._2_8;
+        _2_9 = user._2_9;
+        _2_10 = user._2_10;
+        _2_11 = user._2_11;
+        _3_1 = user._3_1;
+        _3_2 = user._3_2;
+        _3_3 = user._3_3;
+        _3_4 = user._3_4;
+        _3_5 = user._3_5;
+        _3_6 = user._3_6;
+        _3_7 = user._3_7;
+        _3_8 = user._3_8;
+        _3_9 = user._3_9;
+        _3_10 = user._3_10;
+        return user.name;
+    });
+
+    res.render('course.ejs', 
+    {
+        _1_1: _1_1,
+        _1_2: _1_2,
+        _1_3: _1_3,
+        _1_4: _1_4,
+        _1_5: _1_5,
+        _1_6: _1_6,
+        _1_7: _1_7,
+        _1_8: _1_8,
+        _1_9: _1_9,
+        _1_10: _1_10,
+        _1_11: _1_11,
+        _1_12: _1_12,
+        _1_13: _1_13,
+        _1_14: _1_14,
+        _1_15: _1_15,
+        _1_16: _1_16,
+        _2_1: _2_1,
+        _2_2: _2_2,
+        _2_3: _2_3,
+        _2_4: _2_4,
+        _2_5: _2_5,
+        _2_6: _2_6,
+        _2_7: _2_7,
+        _2_8: _2_8,
+        _2_9: _2_9,
+        _2_10: _2_10,
+        _2_11: _2_11,
+        _3_1: _3_1,
+        _3_2: _3_2,
+        _3_3: _3_3,
+        _3_4: _3_4,
+        _3_5: _3_5,
+        _3_6: _3_6,
+        _3_7: _3_7,
+        _3_8: _3_8,
+        _3_9: _3_9,
+        _3_10: _3_10
+    });
+});
+
+// gets list of video titles with their links
+app.post('/videoList', async (req, res) => 
+{
+    let request = Object.keys(JSON.parse(JSON.stringify(req.body)))[0];
+
+    const videoList = await loginCollection.findOne({trackMod: request}).then((data, err) =>
+    {
+        return data;
+    });
+    res.json(videoList);
+});
+
+app.get('/track', async (req, res) =>
+{
+    let videoTime;
+
+    const userName = await loginCollection.findOne({_id: req.user._conditions._id}).then((user, err) =>
+    {
+        userEmail = user.email;
+        _1_1 = user._1_1;
+        _1_2 = user._1_2;
+        _1_3 = user._1_3;
+        _1_4 = user._1_4;
+        _1_5 = user._1_5;
+        _1_6 = user._1_6;
+        _1_7 = user._1_7;
+        _1_8 = user._1_8;
+        _1_9 = user._1_9;
+        _1_10 = user._1_10;
+        _1_11 = user._1_11;
+        _1_12 = user._1_12;
+        _1_13 = user._1_13;
+        _1_14 = user._1_14;
+        _1_15 = user._1_15;
+        _1_16 = user._1_16;
+        _2_1 = user._2_1;
+        _2_2 = user._2_2;
+        _2_3 = user._2_3;
+        _2_4 = user._2_4;
+        _2_5 = user._2_5;
+        _2_6 = user._2_6;
+        _2_7 = user._2_7;
+        _2_8 = user._2_8;
+        _2_9 = user._2_9;
+        _2_10 = user._2_10;
+        _2_11 = user._2_11;
+        _3_1 = user._3_1;
+        _3_2 = user._3_2;
+        _3_3 = user._3_3;
+        _3_4 = user._3_4;
+        _3_5 = user._3_5;
+        _3_6 = user._3_6;
+        _3_7 = user._3_7;
+        _3_8 = user._3_8;
+        _3_9 = user._3_9;
+        _3_10 = user._3_10;
+        return user.name;
+    });
+
+    res.render('track.ejs', 
+    {
+        _1_1: _1_1,
+        _1_2: _1_2,
+        _1_3: _1_3,
+        _1_4: _1_4,
+        _1_5: _1_5,
+        _1_6: _1_6,
+        _1_7: _1_7,
+        _1_8: _1_8,
+        _1_9: _1_9,
+        _1_10: _1_10,
+        _1_11: _1_11,
+        _1_12: _1_12,
+        _1_13: _1_13,
+        _1_14: _1_14,
+        _1_15: _1_15,
+        _1_16: _1_16,
+        _2_1: _2_1,
+        _2_2: _2_2,
+        _2_3: _2_3,
+        _2_4: _2_4,
+        _2_5: _2_5,
+        _2_6: _2_6,
+        _2_7: _2_7,
+        _2_8: _2_8,
+        _2_9: _2_9,
+        _2_10: _2_10,
+        _2_11: _2_11,
+        _3_1: _3_1,
+        _3_2: _3_2,
+        _3_3: _3_3,
+        _3_4: _3_4,
+        _3_5: _3_5,
+        _3_6: _3_6,
+        _3_7: _3_7,
+        _3_8: _3_8,
+        _3_9: _3_9,
+        _3_10: _3_10
+    });
 });
 
 app.listen(3000);
